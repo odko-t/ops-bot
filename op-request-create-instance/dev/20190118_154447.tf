13e2febc2bae4038259b949d0e3cc8f761cac2f7
Od-Erdene <45654790+od-erdene@users.noreply.github.com>
2019-01-18 15:44:47 +0900

    my commit message

---
+++ op-request-create-instance/dev/20190118_154447.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "start-stop-test" {
+							ami = "ami-04d3eb2e1993f679b"
+							instance_type = "t3.micro"
+							subnet_id = "${data.aws_subnet.dev_default.id}"
+							associate_public_ip_address = true
+							vpc_security_group_ids = ["${data.aws_security_group.dev-default.id}", "${data.aws_security_group.allow-ssh-from-intra.id}", "${data.aws_security_group.allow-http-from-intra.id}"]
+							tags {
+									Name = "new-instance-name"
+									datadog = "false" # Set false when this instance
+									Environment = "dev"
+									Department = "ti"
+							}
+						}
